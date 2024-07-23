2909d4c13104e9aa7c670a2a5e71204bbac6fe55
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-07-23 09:39:59 +0700

    chore: initialize

---
+++ apps/web/app/app.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Nuxt to Pages</h1>
+</template>
---
+++ apps/web/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	future: {
+		compatibilityVersion: 4,
+	},
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	nitro: {
+		preset: 'cloudflare-pages',
+	},
+	modules: ['nitro-cloudflare-dev'],
+})
